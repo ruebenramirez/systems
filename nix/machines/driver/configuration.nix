8355629f7f7e3f3c48475263a47b91bd16e58d9e
Rueben Ramirez <ruebenramirez@gmail.com>
2023-11-30 01:22:32 -0600

    reshift setup with manual geocoordinates for ATX

--- nix/machines/driver/configuration.nix
+++ nix/machines/driver/configuration.nix
@@ -265,4 +265,26 @@ in
     polkitPolicyOwners = [ "rramirez" ];
   };
 
+
+  # redshift configuration
+
+  location = {
+	provider = "manual";
+	latitude = 30.266666;
+	longitude = -97.733330;
+  };
+  # All values except 'enable' are optional.
+  services.redshift = {
+    enable = true;
+    brightness = {
+      # Note the string values below.
+      day = "1";
+      night = "1";
+    };
+    temperature = {
+      day = 5500;
+      night = 1500;
+    };
+  };
+
 }
