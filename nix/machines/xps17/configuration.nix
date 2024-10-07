e010c0749b6cae37808931c7d79e79e3ebcf4617
Rueben Ramirez <ruebenramirez@gmail.com>
2024-10-07 11:43:53 -0500

    remove waydroid

--- nix/machines/xps17/configuration.nix
+++ nix/machines/xps17/configuration.nix
@@ -220,8 +220,6 @@ in
     acceleration = "cuda";
   };
 
-  virtualisation.waydroid.enable = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
