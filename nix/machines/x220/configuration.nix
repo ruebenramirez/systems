45383b4fdf31982cb76885f21940c6b1fea30ebb
Rueben Ramirez <ruebenramirez@gmail.com>
2024-10-07 21:31:43 -0500

    x220 nixos update

--- nix/machines/x220/configuration.nix
+++ nix/machines/x220/configuration.nix
@@ -10,16 +10,17 @@ in
   imports =
     [
       ./hardware-configuration.nix
+      ./fingerprint-reader.nix
       ../_common/desktop.nix
       ../_common/base.nix
-      # Import nix-garage
-      #./nix-garage-overlay.nix
-      ./home.nix
+      ../_common/syncthing.nix
     ];
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
 
   # Necessary in most configurations
   nixpkgs.config.allowUnfree = true;
-
 
   nix.settings.trusted-users = [ "rramirez" ];
 
@@ -32,64 +33,36 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "ntfs" ];
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 
-  networking.hostName = "x220"; # Define your hostname.
-  # Need to be set for ZFS or else leads to:
-  # Failed assertions:
-  # - ZFS requires networking.hostId to be set
-  networking.hostId = "6f602d2b";
+  networking = {
+    hostName = "x220";
+    hostId = "6f612d27";
 
-  # Enables wireless support via wpa_supplicant
-  networking.wireless.enable = true;
-  # Option is misleading but we dont want it
-  networking.wireless.userControlled.enable = false;
-  # Allow configuring networks "imperatively"
-  networking.wireless.allowAuxiliaryImperativeNetworks = true;
+    # Remove warning from tailscale: Strict reverse path filtering breaks Tailscale exit node use and some subnet routing setups
+    firewall.checkReversePath = "loose";
+    networkmanager.enable = true;
+  };
+  programs.nm-applet.enable = true;
 
-  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
-
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-
-  # Make sure that dhcpcd doesnt timeout when interfaces are down
-  # ref: https://nixos.org/manual/nixos/stable/options.html#opt-networking.dhcpcd.wait
-  #networking.dhcpcd.wait = "if-carrier-up";
-  #networking.interfaces.enp2s0f0.useDHCP = true;
-  #networking.interfaces.enp5s0.useDHCP = true;
-  networking.interfaces.wlp3s0.useDHCP = true;
-
-  # Leave commented until tether is needed
-  #networking.interfaces.enp7s0f4u2.useDHCP = true;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Audio
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.support32Bit = true;
-
-  # Bluetooth
-  #hardware.bluetooth.enable = true;
-  hardware.bluetooth.enable = false;
-  #services.blueman.enable = true;
-  services.blueman.enable = false;
-
-  # use Fish shell
-  users.defaultUserShell = pkgs.fish;
-  programs.fish.enable = true;
+  # DNS services
+  services.resolved = {
+    enable = true;
+#    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1" "1.0.0.1" ];
+#    extraConfig = ''
+#      DNSOverTLS=yes
+#    '';
+  };
+  services.avahi.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.rramirez = {
     isNormalUser = true;
     uid = 1000;
-    extraGroups = [ "wheel" "audio" "sound" "docker" ]; # Enable ‘sudo’ for the user.
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFeq0/IpNsLCUDVhxRx/wEj4BViCSH/8n4nhD7+PFkzuXpwKft1s5PVFJrlixv7cEJyJTi4FgeeP4N6tPglsIamplfzBjXgRTs0+ssH8ZrHM6l+0jbMqVc39hDRYl78qoxslrz3b0oU4H8bKylyOoEBO9qlJEh4bsIYkUD8ZIbaJa6g3wnPzp/WPjAG76tdoMnuxDQ1uVWph4diQxI85iwnU32anC85w6KthXQABbyV8SAYZvc7vKcN8Mf1JJSGct4nVB/XzZ3mTk3C3L0DA63f6UTtgtCZAXIsqhjeGahp+QUBIgrFG5fG1o5hmHgyHZuOIrbU1BbH/mWpaXbGUut rramirez@le-laptop" ];
+    extraGroups = [ "audio" "docker" "networkmanager" "sound" "wheel" ];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAkQS5ohCDizq24WfDgP/dEOonD/0WfrI0EAZFCyS0Ea" ];
   };
   security.sudo.extraRules = [
     {
@@ -103,6 +76,9 @@ in
     }
   ];
 
+
+  virtualisation.docker.enable = true; # required for k3d
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment = {
@@ -113,8 +89,9 @@ in
       ticker # stocks
       newsboat
       icdiff
-      imagemagick
+      magic-wormhole
       nixpkgs-review
+
       # hardware key
       gnupg
       pcsclite
@@ -139,11 +116,6 @@ in
     };
   };
 
-  # enable iPhone usb tethering for internet access
-  services.usbmuxd.enable = true;
-
-
-
   # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
@@ -160,26 +132,6 @@ in
     ];
   };
 
-  services.cron = {
-    enable = true;
-    # Clean up nixOS generations
-    # NOTE: Still requires a nix-rebuild switch to update grub
-    # List generations: nix-env --list-generations -p /nix/var/nix/profiles/system
-    systemCronJobs = [
-      "0 1 * * * root nix-env --delete-generations +10 -p /nix/var/nix/profiles/system 2>&1 | logger -t generations-cleanup"
-    ];
-  };
-
-  # firmware update
-  services.fwupd.enable = true;
-
-  # Dont start tailscale by default
-  services.tailscale.enable = true;
-  # Remove warning from tailscale: Strict reverse path filtering breaks Tailscale exit node use and some subnet routing setups
-  networking.firewall.checkReversePath = "loose";
-
-  #services.logind.extraConfig = "HandleLidSwitch=ignore";
-
   # part of gnupg reqs
   services.pcscd.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
@@ -187,7 +139,7 @@ in
   # programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
-    pinentryFlavor = "tty";
+    # pinentryFlavor = "tty";
     # Make pinentry across multiple terminal windows, seamlessly
     enableSSHSupport = true;
   };
@@ -202,7 +154,6 @@ in
     '';
   };
   # List services that you want to enable:
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
@@ -220,43 +171,65 @@ in
       monthly = 3;
     };
   };
-
-  virtualisation.docker.enable = true;
-
   systemd.services.zfs-scrub.unitConfig.ConditionACPower = true;
 
-  # fingerprint reader configuration
-  services.fprintd.enable = true;
-  services.fprintd.tod.enable = true;
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
 
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
+  # firmware update
+  services.fwupd.enable = true;
+
+  # laptop power management
+  powerManagement.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      # Optimize for performance while on AC.
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+
+      # Optimize for battery runtime while on battery.
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 100;
+
+      # extend the life of the battery (I'm always plugged in anyways)
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
     };
   };
-
-  # dont hiberate/sleep by default
-  powerManagement.enable = true;
-  # Enable tlp for stricter governance of power management
   # Validate status: `sudo tlp-stat -b`
-  services.tlp.enable = true;
+
+  # if laptop lid closes
+  #services.logind.extraConfig = "HandleLidSwitch=ignore";
+  services.logind.lidSwitch = "suspend-then-hibernate";
+  services.logind.lidSwitchExternalPower = "suspend-then-hibernate";
+
+
+  services.cron = {
+    enable = true;
+    # Clean up nixOS generations
+    # NOTE: Still requires a nix-rebuild switch to update grub
+    # List generations: nix-env --list-generations -p /nix/var/nix/profiles/system
+    systemCronJobs = [
+      "0 1 * * * root nix-env --delete-generations +10 -p /nix/var/nix/profiles/system 2>&1 | logger -t generations-cleanup"
+    ];
+  };
+
+  services.ollama = {
+    enable = false;
+    #acceleration = "cuda";
+  };
+
+  hardware.logitech.wireless.enable = true;
+  hardware.logitech.wireless.enableGraphical = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
