9973810bb38f8abc7fb192c35c5f416a6b511caf
Rueben Ramirez <ruebenramirez@gmail.com>
2024-11-10 09:06:21 -0600

    open-webui for local llm web interface

--- nix/machines/_common/local-llm.nix
+++ nix/machines/_common/local-llm.nix
@@ -9,10 +9,15 @@ in
     ollama
   ];
 
-
   services.ollama = {
     enable = true;
     acceleration = "cuda";
+  };
+
+  services.open-webui = {
+    enable = true;
+    host = "0.0.0.0";
+    port = 8888;
   };
 
 }
