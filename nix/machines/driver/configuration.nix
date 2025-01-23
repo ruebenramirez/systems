bda8fb7187c19ed058015673280500f977ea4026
Rueben Ramirez <ruebenramirez@gmail.com>
2025-01-23 09:54:39 -0600

    remove unneeded dns config

--- nix/machines/driver/configuration.nix
+++ nix/machines/driver/configuration.nix
@@ -54,12 +54,8 @@ in
   # DNS services
   services.resolved = {
     enable = true;
-#    dnssec = "true";
     domains = [ "~." ];
     fallbackDns = [ "1.1.1.1" "1.0.0.1" ];
-#    extraConfig = ''
-#      DNSOverTLS=yes
-#    '';
   };
   services.avahi.enable = true;
 
