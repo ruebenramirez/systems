3780ab584714ef9a4cfa507bf292fccacbccbd59
Rueben Ramirez <ruebenramirez@gmail.com>
2025-02-18 01:03:25 -0600

    docker networking is working finally

--- nix/machines/_common/kubernetes.nix
+++ nix/machines/_common/kubernetes.nix
@@ -7,7 +7,16 @@ in
 {
 
 
-  virtualisation.docker.enable = true; # required for k3d
+  virtualisation.docker = {
+    enable = true; # required for k3d
+    enableOnBoot = true;
+    extraOptions = "--iptables=true --ip-masq=true --ip-forward=true";
+  };
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.conf.all.forwarding" = 1;
+  };
+  networking.firewall.trustedInterfaces = [ "docker0" ];
 
   environment.systemPackages = with pkgs; [
       k3d # k3s on docker (docker required)
