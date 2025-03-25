de11853fc58d335e559fd682e21fd5164cb01d18
Rueben Ramirez <ruebenramirez@gmail.com>
2025-03-25 15:03:35 -0500

    docker service installed but disabled by default

--- nix/machines/_common/kubernetes.nix
+++ nix/machines/_common/kubernetes.nix
@@ -12,6 +12,15 @@ in
     enableOnBoot = true;
     extraOptions = "--iptables=true --ip-masq=true --ip-forward=true";
   };
+
+  # disable docker systemd service by default (must start prior to using docker)
+  # systemd.services.docker.enable = false; # masks the service (doesn't allow manual starts)
+  # Disable automatic start of Docker service
+  systemd.services.docker.wantedBy = pkgs.lib.mkForce [];
+
+  # Ensure the service is not masked
+  systemd.services.docker.unitConfig.AssertPathExists = "/run/booted-system";
+
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
