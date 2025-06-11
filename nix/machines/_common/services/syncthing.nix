868687af36bdff22e2ca502d20c52cf684eddd95
Rueben Ramirez <ruebenramirez@gmail.com>
2025-06-11 08:15:01 -0500

    default syncthing configuration for gui on loopback

--- nix/machines/_common/services/syncthing.nix
+++ nix/machines/_common/services/syncthing.nix
@@ -15,6 +15,7 @@ in
   services = {
       syncthing = {
           enable = true;
+          guiAddress = "127.0.0.1:8384";
           user = "rramirez";
           dataDir = "/home/rramirez/Sync";    # Default folder for new synced folders
           configDir = "/home/rramirez/.config/syncthing";   # Folder for Syncthing's settings and keys
