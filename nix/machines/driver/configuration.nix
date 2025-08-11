31c241e1dd15d320a1ad8a2f4532495f94611374
Rueben Ramirez <ruebenramirez@gmail.com>
2025-08-11 05:22:19 -0500

    allow remote development of hugo blog over 1313
    I'm daily driving a macbook air and doing all development through a
    terminal (nixos on thinkpad is primary dev env) and browser.

--- nix/machines/driver/configuration.nix
+++ nix/machines/driver/configuration.nix
@@ -145,7 +145,7 @@ in
   };
   # List services that you want to enable:
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 1313 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
