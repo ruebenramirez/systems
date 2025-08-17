9fd872d3c291b97cab856a4e62e5e81369afdeeb
Rueben Ramirez <ruebenramirez@gmail.com>
2025-08-17 12:28:57 -0500

    remove unused dns config

--- nix/machines/xps17/configuration.nix
+++ nix/machines/xps17/configuration.nix
@@ -35,12 +35,8 @@
   # DNS services
   services.resolved = {
     enable = true;
-#    dnssec = "true";
     domains = [ "~." ];
     fallbackDns = [ "1.1.1.1" "1.0.0.1" ];
-#    extraConfig = ''
-#      DNSOverTLS=yes
-#    '';
   };
   services.avahi.enable = true;
 
