299b5da0351094ddb40e3671278db9e32637e3c4
Rueben Ramirez <ruebenramirez@gmail.com>
2025-08-26 16:09:52 -0500

    jellyfin AMD hardware acceleration configured

--- nix/machines/homeserver/srv/jellyfin.nix
+++ nix/machines/homeserver/srv/jellyfin.nix
@@ -5,6 +5,37 @@ let
 
 in
 {
+
+
+  # Enable graphics hardware acceleration
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      # VA-API support for AMD GPUs
+      vaapiVdpau
+      libvdpau-va-gl
+      # OpenCL support for AMD GPUs (required for tone mapping and subtitle burn-in)
+      rocmPackages.clr.icd
+    ];
+  };
+
+  # Set VA-API driver for AMD
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "radeonsi";
+  };
+
+  # Configure Jellyfin service with hardware acceleration environment
+  systemd.services.jellyfin = {
+    environment = {
+      LIBVA_DRIVER_NAME = "radeonsi";
+    };
+    # Ensure jellyfin user has access to render group for GPU access
+    serviceConfig = {
+      SupplementaryGroups = [ "render" "video" ];
+    };
+  };
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
@@ -15,5 +46,12 @@ in
     jellyfin
     jellyfin-web
     jellyfin-ffmpeg
+    libva-utils  # provides vainfo command
+    clinfo       # provides clinfo command for OpenCL verification
   ];
+
+  # Add user to required groups for GPU access
+  users.users.rramirez = {
+    extraGroups = [ "video" "render" ];
+  };
 }
