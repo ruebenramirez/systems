7c7b0411e11d80d70fa9b18b90083798d6fdebe2
Rueben Ramirez <ruebenramirez@gmail.com>
2025-10-29 20:30:40 -0500

    allow homeserver wgnet access clients with mullvad generated IPs
    Some mobile devices do not allow more than a single vpn interface to
    remain active at a time.  Because of this, it's easier to add a wgnet
    peer to the mullvad wireguard interface.  (this allows personal
    wireguard subnet access while also having mullvad access.

--- nix/machines/homeserver/wireguard.nix
+++ nix/machines/homeserver/wireguard.nix
@@ -42,56 +42,20 @@
 
           # Route only mesh network traffic through WireGuard (split-tunnel)
           # To route all traffic through VPN, use: [ "0.0.0.0/0" ]
-          allowedIPs = [ "10.100.0.0/24" ];
+          allowedIPs = [
+            "10.100.0.0/24"     # normal wgnet subnet
+            "10.75.40.234/32"   # rueben phone mullvad vpn ip
+            "10.71.234.205/32"  # Monica phone mullvad vpn ip
+            "10.65.96.166/32"   # Monica laptop mullvad vpn ip
+          ];
 
           # Keep connection alive through NAT
-          persistentKeepalive = 25;
-        }
-        # # rueben-tablet
-        # {
-        #   publicKey = "cBuVlcnqaG3RR/x41CgXMfWYon/ZaTtrYKw0sbILmjs=";
-        #   allowedIPs = [ "10.100.0.9/32" ];
-        #   persistentKeepalive = 25;
-        # }
-        # download server wireguard peer configuration
-        #   mullvad generated this priv/pub keypair and IP address
-        {
-          publicKey = "4zjTZcpC/6cMaqAQPIwOJctZGF6+rixMzOBS6HW9B1Y=";
-          allowedIPs = [ "10.68.29.189/32" ];
           persistentKeepalive = 25;
         }
         # gl.inet travel router
         {
           publicKey = "2tdNHRdU6/QQyuwW8gnGjJsxsyLuOEYGZE3D90niOg4=";
           allowedIPs = [ "10.100.0.10/32" ];
-          persistentKeepalive = 25;
-        }
-        # driver wireguard peer configuration
-        #   mullvad generated this priv/pub keypair and IP address
-        {
-          publicKey = "AMg/JU3QGUC/DXQorXG2PDBf5iuTFmIxvlPxyp5HuSg=";
-          allowedIPs = [ "10.69.137.165/32" ];
-          persistentKeepalive = 25;
-        }
-        # rueben phone wireguard peer configuration
-        #   mullvad generated this priv/pub keypair and IP address
-        {
-          publicKey = "zAiA1+I9EkDdkRmtygzI/A7XaVoX2ZWB4nfrog9gVFw=";
-          allowedIPs = [ "10.75.40.234/32" ];
-          persistentKeepalive = 25;
-        }
-        # monica phone wireguard peer configuration
-        #   mullvad generated this priv/pub keypair and IP address
-        {
-          publicKey = "qpVKvBNmPJ77UrnHyW77Bd0NoLMPqHgR27OPE+al3xQ=";
-          allowedIPs = [ "10.71.234.205/32" ];
-          persistentKeepalive = 25;
-        }
-        # monica laptop wireguard peer configuration
-        #  mullvad wireguard generated this priv/pub keypair and IP address
-        {
-          publicKey = "4WVHjD+sjHhO5x2MXTRTKeAY/YcyB9ZTevZ+iTkw/gM=";
-          allowedIPs = [ "10.65.96.166/32" ];
           persistentKeepalive = 25;
         }
         # Carolyn laptop wireguard peer configuration
