ce431b81bdf5d328b08ba95d07e45ecf11926c4f
Rueben Ramirez <ruebenramirez@gmail.com>
2025-12-15 20:56:31 -0600

    neovim lspconfig deprecation

--- nix/machines/_common/base/editor.nix
+++ nix/machines/_common/base/editor.nix
@@ -308,7 +308,6 @@
         -- ========================================
         -- LSP CONFIGURATION
         -- ========================================
-        local lspconfig = require('lspconfig')
 
         -- Enhanced capabilities with nvim-cmp
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -333,36 +332,6 @@
             prefix = "",
           },
         })
-
-        -- LSP keymaps (applied when LSP attaches)
-        local on_attach = function(client, bufnr)
-          local opts = { noremap = true, silent = true, buffer = bufnr }
-
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', '<leader>f', function()
-            vim.lsp.buf.format { async = true }
-          end, opts)
-        end
-
-        -- Setup Nix language server
-        lspconfig.nil_ls.setup {
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = {
-            ['nil'] = {
-              formatting = {
-                command = { "nixfmt" },
-              },
-            },
-          },
-        }
 
         -- ========================================
         -- NVIM-CMP CONFIGURATION
