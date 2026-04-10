993fdbcc4bdeb4e728f3bbb3964d6a2498b3b606
Rueben Ramirez <ruebenramirez@gmail.com>
2026-04-10 17:56:32 -0500

    jellyfin reverse proxy

--- nix/machines/homeserver/srv/jellyfin.nix
+++ nix/machines/homeserver/srv/jellyfin.nix
@@ -54,4 +54,31 @@ in
   users.users.rramirez = {
     extraGroups = [ "video" "render" ];
   };
+
+
+  # Grant Nginx access to read the certificate owned by the shared group
+  users.users.nginx.extraGroups = [ "ruebdev-wildcard-tls" ];
+
+  # nginx reverse proxy
+  services.nginx = {
+    enable = true;
+
+    # Recommended default settings
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+
+    virtualHosts."tv.rueb.dev" = {
+      forceSSL = true;
+
+      # Use the wildcard cert defined in acme-wildcard.nix
+      useACMEHost = "rueb.dev";
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8096";
+        proxyWebsockets = true;
+      };
+    };
+  };
 }
