1956f107b9e104b53e37c9ccdb81eab3f07f3631
Rueben Ramirez <ruebenramirez@gmail.com>
2026-04-10 18:03:52 -0500

    reverse proxy for audiobookshelf

--- nix/machines/homeserver/srv/audiobookshelf.nix
+++ nix/machines/homeserver/srv/audiobookshelf.nix
@@ -8,8 +8,33 @@ in
     enable = true;
     host = "0.0.0.0";
     port = 13378;
-    openFirewall = true;
     package = pkgs-unstable.audiobookshelf;
     user = "rramirez";
   };
+
+  # Grant Nginx access to read the certificate owned by the shared group
+  users.users.nginx.extraGroups = [ "ruebdev-wildcard-tls" ];
+
+  services.nginx = {
+    enable = true;
+
+    # Recommended default settings
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+
+    virtualHosts."audiobooks.rueb.dev" = {
+      forceSSL = true;
+
+      # Use the wildcard cert defined in acme-wildcard.nix
+      useACMEHost = "rueb.dev";
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:13378";
+        proxyWebsockets = true;
+      };
+    };
+  };
+
 }
