4fa19973424222164ab93a665ed63ba60f6a3f0a
Rueben Ramirez <ruebenramirez@gmail.com>
2026-04-13 12:36:08 -0500

    renable mullvad on driver thinkpad

--- nix/machines/driver/configuration.nix
+++ nix/machines/driver/configuration.nix
@@ -12,7 +12,7 @@
       ../_common/gaming.nix
       ../_common/gpu-amd.nix
       ../_common/home-vpn-client.nix
-      #../_common/mullvad-client.nix
+      ../_common/mullvad-client.nix
       ../_common/rust-dev.nix
       ./hardware-configuration.nix
       ./mtp-storage-access.nix
