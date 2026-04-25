72fe431de3525cd529a7de7d77ce562908f157d2
Rueben Ramirez <ruebenramirez@gmail.com>
2026-04-24 21:29:49 -0500

    xreal udev comment cleanup

--- nix/machines/driver/udev-rules/xreal-udev-unplug-restart-kanshi.nix
+++ nix/machines/driver/udev-rules/xreal-udev-unplug-restart-kanshi.nix
@@ -3,15 +3,15 @@
 let
   # Simple script to trigger a full Sway reload
   swayReload = pkgs.writeShellScript "sway-reload-script" ''
-    # 1. Wait 2 seconds for the hardware state to settle
+    # Wait 2 seconds for the hardware state to settle
     sleep 2
 
-    # 2. Find the Sway socket for your user
+    # Find the Sway socket for your user
     SWAY_PID=$( ${pkgs.procps}/bin/pgrep -u 1000 -x sway )
     if [ -n "$SWAY_PID" ]; then
       export SWAYSOCK="/run/user/1000/sway-ipc.1000.$SWAY_PID.sock"
 
-      # 3. Issue a full reload command
+      # Issue a full reload command
       ${pkgs.sway}/bin/swaymsg reload
 
       echo "$(date): Sway reload triggered via XREAL removal" >> /tmp/kanshi.log
