3b1c8715cae82f1e565d62e37dac707911819d6b
Rueben Ramirez <ruebenramirez@gmail.com>
2026-05-19 10:41:15 -0500

    kubernetes.nix cleanup

--- nix/machines/_common/services/kubernetes.nix
+++ nix/machines/_common/services/kubernetes.nix
@@ -1,38 +1,24 @@
-
 { config, pkgs, ... }:
-
-let
-
-in
 {
-
-
-  virtualisation.docker = {
-    enable = true; # required for k3d
-    enableOnBoot = true;
-    extraOptions = "--iptables=true --ip-masq=true --ip-forward=true";
-  };
-
-  # disable docker systemd service by default (must start prior to using docker)
-  # systemd.services.docker.enable = false; # masks the service (doesn't allow manual starts)
-  # Disable automatic start of Docker service
-  systemd.services.docker.wantedBy = pkgs.lib.mkForce [];
-
-  # Ensure the service is not masked
-  systemd.services.docker.unitConfig.AssertPathExists = "/run/booted-system";
-
-  # TODO: resolve conflict with wireguard configuration
-  # boot.kernel.sysctl = {
-  #   "net.ipv4.ip_forward" = 1;
-  #   "net.ipv6.conf.all.forwarding" = 1;
-  # };
-  networking.firewall.trustedInterfaces = [ "docker0" ];
-
   environment.systemPackages = with pkgs; [
       k3d # k3s on docker (docker required)
       kubectl
       kubernetes-helm
       helmfile-wrapped
   ];
+
+  # k3d requires docker
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    extraOptions = "--iptables=true --ip-masq=true --ip-forward=true";
+  };
+
+  # Disable automatic start of Docker service
+  #systemd.services.docker.wantedBy = pkgs.lib.mkForce [];
+
+  # Ensure the service is not masked
+  systemd.services.docker.unitConfig.AssertPathExists = "/run/booted-system";
+
 
 }
