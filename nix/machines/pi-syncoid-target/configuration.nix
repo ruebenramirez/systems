6393642ba44632e1cdb285e5a3aea4bd601096b1
Rueben Ramirez <ruebenramirez@gmail.com>
2026-06-23 17:49:26 -0500

    relocate ssh host key for sops-nix usage

--- nix/machines/pi-syncoid-target/configuration.nix
+++ nix/machines/pi-syncoid-target/configuration.nix
@@ -80,14 +80,24 @@
     nftables.enable = true;
   };
 
-  # SSH configuration
   services.openssh = {
     enable = true;
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
     };
-   };
+    hostKeys = [
+      {
+        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+      {
+        path = "/persist/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+        bits = 4096;
+      }
+    ];
+  };
 
   # Native NixOS Garbage Collection
   nix.gc = {
