e7945421f24bb72969749755e4450fbe291f4429
Rueben Ramirez <ruebenramirez@gmail.com>
2026-06-29 15:18:28 -0500

    clean up cachix config

--- nix/machines/_common/base/nix-cache.nix
+++ nix/machines/_common/base/nix-cache.nix
@@ -2,11 +2,9 @@
   # append rather than overwrite default caches
   nix.settings.extra-substituters = [
     "https://nix-community.cachix.org"
-    "https://nixos-raspberrypi.cachix.org"
   ];
 
   nix.settings.extra-trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
   ];
 }
