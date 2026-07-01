546df7c9fc7b560021c41b1bb5ab8ff6bb57f23a
Rueben Ramirez <ruebenramirez@gmail.com>
2026-07-01 02:20:22 -0500

    add flake support back
    oops

--- nix/machines/_common/base/nix-settings.nix
+++ nix/machines/_common/base/nix-settings.nix
@@ -1,6 +1,10 @@
 {
   time.timeZone = "America/Chicago";
 
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
   nix.settings.trusted-users = [ "rramirez" ];
 
   nix.gc = {
