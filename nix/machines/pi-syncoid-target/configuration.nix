b52fdb945f86ae15557004c18857876efa0c6f08
Rueben Ramirez <ruebenramirez@gmail.com>
2026-06-30 22:52:18 -0500

    cleanup pi build cache settings

--- nix/machines/pi-syncoid-target/configuration.nix
+++ nix/machines/pi-syncoid-target/configuration.nix
@@ -14,9 +14,6 @@
   nix = {
     settings = {
       trusted-users = [ "rramirez" ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
     };
     extraOptions = ''
       experimental-features = nix-command flakes
