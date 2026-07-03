ad38f2ff2cdfa780f473d46a43fc3b328a464692
Rueben Ramirez <ruebenramirez@gmail.com>
2026-07-03 11:14:09 -0500

    formatting for readability

--- nix/machines/_common/base/ssh.nix
+++ nix/machines/_common/base/ssh.nix
@@ -1,10 +1,13 @@
 {
   services.openssh = {
+
     enable = true;
+
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
     };
+
     hostKeys = [
       {
         path = "/persist/etc/ssh/ssh_host_ed25519_key";
