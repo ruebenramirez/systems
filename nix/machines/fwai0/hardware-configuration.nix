788a87794dc385a9ab19a75fbd3d8814e2060031
Rueben Ramirez <ruebenramirez@gmail.com>
2026-07-16 17:02:27 -0500

    fwai0 to 6.18.x kernel packaging

--- nix/machines/fwai0/hardware-configuration.nix
+++ nix/machines/fwai0/hardware-configuration.nix
@@ -9,7 +9,7 @@
     ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_7_0;
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
   boot.zfs.package = pkgs.zfs_2_4;
   boot.initrd.availableKernelModules = [
     "nvme"
