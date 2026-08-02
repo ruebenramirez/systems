411743466a03e85b0a90e6e3c34e7c4e657674b8
Rueben Ramirez <ruebenramirez@gmail.com>
2026-08-02 16:23:32 -0500

    remove unused sunshine game streaming nix module

--- nix/machines/_common/sunshine-game-streaming.nix
+++
@@ -1,9 +0,0 @@
-{
-  # Enable Sunshine host for Moonlight clients
-  services.sunshine = {
-    enable = true;
-    autoStart = true;
-    capSysAdmin = true; # Required for KMS capture
-    openFirewall = true;
-  };
-}
