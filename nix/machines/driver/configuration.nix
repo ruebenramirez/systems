e98119755ffc55978c300f5f2adb2b9b2605e032
Rueben Ramirez <ruebenramirez@gmail.com>
2026-09-01 11:21:16 -0500

    tailscale client install

--- nix/machines/driver/configuration.nix
+++ nix/machines/driver/configuration.nix
@@ -12,6 +12,7 @@
       ../_common/gpu-amd.nix
       ../_common/home-vpn-client.nix
       ../_common/mullvad-vpn-client.nix
+      ../_common/tailscale-client.nix
       ../_common/rust-dev.nix
       ../_common/physical.nix
       ../_common/desktop/razer-keyboard.nix
